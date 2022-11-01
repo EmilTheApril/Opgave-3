8ee3ee1439b5bd34c04fd8aee815a1293633d205
EmilTheApril <pjuske.2002@hotmail.com>
2022-11-01 15:03:46 +0100

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    void Start()
+    {
+        //Sets the rigidbody
+        rb = GetComponent<Rigidbody2D>();
+
+        //Sets the animator to the first childs animator
+        anim = transform.GetChild(0).GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        //Calls the Move() function
+        Move();
+    }
+
+    public void Move()
+    {
+        //Gets the horizontal input (-1 to 1)
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            //Sets the velocity of the player
+            rb.velocity = new Vector2(Vector2.right.x * horizontal * speed * Time.deltaTime, rb.velocity.y);
+
+            //Rotates the player
+            transform.localScale = new Vector3(horizontal, 1, 1);
+
+            //Sets isWalking to true, swithing to walk animation
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            //Set the velocity to zero, and keeps the vertical speed/velocity
+            rb.velocity = new Vector2(0, rb.velocity.y);
+
+            //Sets isWalking to false, swithing to idle animation
+            anim.SetBool("isWalking", false);
+        }
+    }
+}
